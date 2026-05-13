a5ba1b167e487f4056eac97a87b1bd837ca80cda
Abdou-Cloud <ab.abdou1800@gmail.com>
2026-05-13 06:03:05 +0200

    update source in invoice_request_facturatie.xsd 3.0

--- src/sender.js
+++ src/sender.js
@@ -303,7 +303,7 @@ class CRMSender {
   address.ele('country').txt(data.address?.country || '');
 
   if (data.customer?.vat_number) invoiceData.ele('vat_number').txt(data.customer.vat_number); // ✅ geen company_name
-  if (data.customer?.company_name) invoiceData.ele('company_name').txt(data.customer.company_name);
+  
   return root.doc().end({ prettyPrint: true, indent: '  ' });
 }
 
