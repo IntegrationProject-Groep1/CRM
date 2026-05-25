bc5d6e79f73170408324c3d7247f16ea4539ddd9
Abdou-Cloud <ab.abdou1800@gmail.com>
2026-05-25 13:33:26 +0200

    fix uuid toevoegen in mail recipient

--- src/receiver.js
+++ src/receiver.js
@@ -1108,7 +1108,7 @@ class ReceiverV2 {
       await this.sender.sendMailingSend({
         correlation_id: header.correlation_id || header.message_id,
         template_id:    'company_invite',
-        recipients:     [{ email: inviteeEmail }],
+        recipients:     [{ email: inviteeEmail, identity_uuid: inviteeUuid }],
         template_data:  JSON.stringify({
           invite_link:  inviteLink,
           expires_at:   expiresAt,
